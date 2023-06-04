f9485db5a6cc4394d12ff6065c66cfeff0874437
Jixian Wang <hellojixian@gmail.com>
2023-06-04 14:49:21 +0300

    update

--- scripts/update_models_gender.py
+++ scripts/update_models_gender.py
@@ -3,6 +3,7 @@ import os
 import sys
 import tqdm
 import pickle
+import numpy as np
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,15 +14,18 @@ import core.data_schema.model as model
 from core.database import Database
 db = Database()
 
-# genders = ['male', 'female', 'shemale']
-genders = ['male']
+name_file = os.path.join(os.environ['PROJECT_FACEDB_PATH'], "gender_names.pickle")
+with open(name_file, 'rb') as f:
+  names = pickle.load(f)
+records = 0
+for gender in names.keys():
+  r = len(names[gender])
+  records += r
+  print(f"{gender} => {r} records")
+print(f"total {records} records loaded")
 
-for gender in genders:
-  name_file = os.path.join(os.environ['PROJECT_FACEDB_PATH'],f"{gender}_names.pickle")
-  with open(name_file, 'rb') as f:
-    names = pickle.load(f)
-
-  for model_name in tqdm.tqdm(names, desc=f'Updating {gender} models'):
+for gender in names.keys():
+  for model_name in tqdm.tqdm(names[gender], desc=f'Updating {gender} models'):
     model_data = model.Model.objects(name=model_name).first()
     if not model_data: continue
     model_data.gender = 'Male'
