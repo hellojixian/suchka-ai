6c164e9bf2d001bee2f61119c8e83d76ae07b5eb
Jixian Wang <hellojixian@gmail.com>
2023-06-09 06:02:04 +0300

    update

--- scripts/load_galleries_to_db.py
+++ scripts/load_galleries_to_db.py
@@ -23,6 +23,7 @@ data_folder = f"{project_folder}/{sys.argv[1]}"
 processed_galleries = set()
 existing_galleries = model.Gallery.objects()
 for gallery in existing_galleries: processed_galleries.add(gallery.gid)
+print(f"Found {len(processed_galleries)} galleries in the database")
 
 def normalize_name(name):
   return " ".join([n.capitalize() for n in name.strip().split(' ')])
@@ -31,22 +32,32 @@ skiped_galleries_no_json = 0
 # list of all subfolders in the input folder
 galleries = set()
 folders = [f for f in os.listdir(data_folder) if os.path.isdir(os.path.join(data_folder, f))]
-for f in folders: galleries.add(f)
+for f in folders: galleries.add(f);
 # sort galleries by name
-galleries = sorted(galleries, key=lambda f: f.name)
+galleries = sorted(galleries, key=lambda f: f)
 for gallery in tqdm.tqdm(galleries, desc="Scanning galleries"):
   gid = gallery.split('/')[-1]
-  if gid in processed_galleries:continue
+  if gid in processed_galleries: continue
 
   # read and parse the data.json file from the gallery folder
-  data_file = f"{gallery}/data.json"
+  data_file = f"{data_folder}/{gallery}/data.json"
   if not os.path.exists(data_file): skiped_galleries_no_json+=1; continue
 
   try:
-    data = json.load(open(data_file))
+    with open(data_file, 'r') as f:
+      data = json.load(f)
   except:
     print(f"Failed to parse {data_file}")
     continue
+
+  # fix data type for some fields
+  if data['copyright'] is None:
+    data['copyright'] = ""
+    with open(data_file, 'w') as json_file:
+      json_file.write(json.dumps(data))
+
+  # !!! check if the gallery is already in the database
+  if ',' not in data['copyright']: continue
 
   gallery_models = data['models'].split(', ') if data['models'] else []
   existing_models = model.Model.objects(name__in=gallery_models)
@@ -65,7 +76,7 @@ for gallery in tqdm.tqdm(galleries, desc="Scanning galleries"):
       existing_models.append(new_model)
 
   gallery_tags = data['tags'].split(', ') if data['tags'] else []
-  existing_tags = model.Tag.objects(name__in=gallery_models)
+  existing_tags = model.Tag.objects(name__in=gallery_tags)
   existing_tags = [t for t in existing_tags]
   existing_tag_names = [m.name for m in existing_tags]
   for tag_name in gallery_tags:
